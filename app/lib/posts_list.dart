e10e95b584b97c0d908cbceeb2365fb002ae6918
caner <caner.capkin@adesso.de>
2019-05-12 20:52:24 +0200

    make post items extractable

---
+++ app/lib/posts_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'post_item.dart';
+
+class PostsList extends StatelessWidget {
+  const PostsList();
+
+  static const List<int> _items = <int>[0, 1, 2];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: _itemList());
+  }
+
+  List<PostItem> _itemList() {
+    return _items.map((int index) => const PostItem()).toList(); 
+  }
+}
